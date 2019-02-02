0e0f6bff92e84df231b62f7f02d867e6aa075b0b
shalini1995 <34934102+shalini1995@users.noreply.github.com>
2019-02-02 19:12:38 +0530

    maintenance

--- app.js
+++ app.js
@@ -12,9 +12,9 @@ hbs.registerHelper('getCurrentYear', () => {
 
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
-app.use((req, res, next) => {
-  res.render('maintanance.hbs');
-});
+// app.use((req, res, next) => {
+//   res.render('maintanance.hbs');
+// });
 
 app.get('/', (req, res) => {
   res.render('home.hbs', {
