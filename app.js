de6b7d8555b4e8eb32f1d025845503a212a2b5e9
shalini1995 <34934102+shalini1995@users.noreply.github.com>
2019-02-02 17:06:46 +0530

    FirstCommit

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const hbs = require('hbs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partial');
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+app.use((req, res, next) => {
+  res.render('maintanance.hbs');
+});
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    page:'Home page',
+    date:new Date().getFullYear()
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    page:'About page',
+    date:new Date().getFullYear()
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server has been started well');
+});
